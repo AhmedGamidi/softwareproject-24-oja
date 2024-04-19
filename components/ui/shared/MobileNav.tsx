306df58c779cb2d5d0c9b4828c3cbc5ceebc6680
Mayokun <sofoworamayokun@gmail.com>
2024-04-19 12:16:09 +0200

    added nav bar system for desktop and mobile view

--- components/ui/shared/MobileNav.tsx
+++ components/ui/shared/MobileNav.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+import Image from "next/image"
+import { Separator } from "../separator"
+import NavItems from "./NavItems"
+  
 
 const MobileNav = () => {
   return (
-    <div>MobileNav</div>
+    <nav className="md:hidden">
+        <Sheet>
+            <SheetTrigger className="align-middle">
+                <Image 
+                src={'/assets/icons/menu.svg'}
+                alt="menu"
+                width={24}
+                height={24}
+                className="cursor-pointer"/>
+            </SheetTrigger>
+            <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+                <Image 
+                src={'/assets/images/linkup_logo.svg'}
+                alt="logo"
+                width={58}
+                height={38}/>
+                <Separator className="border border-gray-50"/>
+                <NavItems />
+            </SheetContent>
+        </Sheet>
+    </nav>
   )
 }
 
