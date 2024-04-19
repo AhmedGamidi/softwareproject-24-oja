b15504ecb36e3f32f4c9440e5a3159dfe9072446
Mayokun <sofoworamayokun@gmail.com>
2024-04-19 15:17:30 +0200

    added home with hero and some texts, should change later

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,42 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      
-    </main>
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
+        <div className="wrapper grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="h1-bold">
+              Host, Connect, Discover, Experience: Your Events, Your Way! 
+            </h1>
+            <p className="p-regular-20 md:p-regular-24">
+              Find and book concerts, conferences, and workshops with ease.
+            </p>
+            <Button size={'lg'} asChild className="button w-full sm:w-fit">
+              <Link href={'#events'}>
+                Explore now
+              </Link>
+            </Button>
+          </div>
+          <Image src={'/assets/images/hero.png'}
+          alt="hero" 
+          width={1000} 
+          height={1000} 
+          className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]" />
+        </div>
+      </section>
+
+      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+        <h2 className="h2-bold">
+          Trusted by <br/> Thousands of events.
+        </h2>
+        <div className="flex w-full flex-col gap05 md:flex-row">
+          Search
+          CategoryFilter
+        </div>
+      </section>
+    </>
   )
 }
--- components/ui/shared/Footer.tsx
+++ components/ui/shared/Footer.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='border-t'>
-      <div className='flex flex-center wrapper flex-between flex-col gap-4 p-5 text-center sm:flex-row'>
+      <div className='flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row'>
         <Link href={'/'}>
           <Image 
           src={'/assets/images/linkup_logo.svg'} 
