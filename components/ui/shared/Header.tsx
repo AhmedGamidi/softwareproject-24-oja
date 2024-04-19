d05fbd180c2c27b30fadd47ac3a2c43baf024bea
Mayokun <sofoworamayokun@gmail.com>
2024-04-19 11:34:35 +0200

    Login pages updated

--- components/ui/shared/Header.tsx
+++ components/ui/shared/Header.tsx
@@ -1,18 +1,31 @@
 
-import { SignedOut } from '@clerk/nextjs'
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { Button } from '../button'
+import NavItems from './NavItems'
+import MobileNav from './MobileNav'
 
 const Header = () => {
   return (
     <header className='w-full border-b'>
       <div className='wrapper flex items-center justify-between'>
         <Link href="/" className='w-36'>
-          <Image src="/assets/images/LinkUp_logo1.svg" width={128} height={38} alt='LinkUp logo'/>
+          <Image src="/assets/images/linkup_logo.svg" width={58} height={38} alt='LinkUp logo'/>
         </Link>
+        
+        <SignedIn>
+          <nav className='md:flex-between hidden w-full max-w-xs'>
+            <NavItems />
+          </nav>
+        </SignedIn>
+
         <div className='flex w-32 justify-end gap-3'>
+          <SignedIn>
+            <UserButton afterSignOutUrl='/'/>
+            <MobileNav />
+          </SignedIn>
           <SignedOut>
             <Button asChild className='rounded-full' size={'lg'}>
               <Link href={'/sign-in'}>
