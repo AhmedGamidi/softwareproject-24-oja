12758ed3131638631d9cc2cf785423bb16a9a7e6
Mayokun-Sofowora <sofoworamayokun@gmail.com>
2024-06-17 15:35:41 +0200

    Update files

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -3,12 +3,12 @@
 import Stripe from 'stripe';
 import { CheckoutOrderParams, CreateOrderParams, GetOrdersByEventParams, GetOrdersByUserParams } from "@/types"
 import { redirect } from 'next/navigation';
-import { handleError } from '../utils';
-import { connectToDatabase } from '../database';
-import Order from '../database/models/order.model';
-import Event from '../database/models/event.model';
 import {ObjectId} from 'mongodb';
+import { connectToDatabase } from '../database';
+import { handleError } from '../utils';
+import Order from '../database/models/order.model';
 import User from '../database/models/user.model';
+import Event from '../database/models/event.model';
 
 export const checkoutOrder = async (order: CheckoutOrderParams) => {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
